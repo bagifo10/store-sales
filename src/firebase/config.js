d2c18800a837809914cef518da4b1ab1f0d6be6a
bauugf <baugimenez2708@gmail.com>
2026-04-09 23:24:43 -0300

    Update storageBucket to appspot.com

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDEB7AhLzLRYO0pmHm3XFR1uPEIo6Zsj3Q",
     authDomain: "shop-689bc.firebaseapp.com",
     projectId: "shop-689bc",
-    storageBucket: "shop-689bc.firebasestorage.app",
+    storageBucket: "shop-689bc.appspot.com",
     messagingSenderId: "429258787233",
     appId: "1:429258787233:web:aae7d9792caa0a7f1a23e0",
     measurementId: "G-93WTDCTB2X"
